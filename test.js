ecfa6b1f1e5495ad144b83006331d7f538a5b565
Rihards <rihards.jukna@gmail.com>
2020-07-29 12:18:28 +0100

    frame for test functions is added for each user story and stretch goal

---
+++ test.js
@@ -0,0 +1,20 @@
+// Test 1. Add tasks to a list so that I can keep track of them.
+test("Submitting a new task adds it to the list", t => {
+    // test goes here
+  });
+
+// Test 2. Check things off my list so that I can see what I’ve done.
+test("Checking an entry marks it as complete", t => {
+    // test goes here
+  });
+
+// Test 3. Delete things from the list if I don’t need to do them anymore
+test("Deleting an entry removes it from the list", t => {
+    // test goes here
+  });
+
+
+// Stretch. Filter out completed to-dos from my list so that I can focus on what's left to do.
+test("Toggling the filter hides completed tasks from the list", t => {
+    // test goes here
+  });
