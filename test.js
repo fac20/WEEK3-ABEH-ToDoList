76764a8768d0e3b11c9faea082896b4305c5097e
Azizi Adeyemo <azizi.adeyemo@gmail.com>
2020-07-30 11:19:20 +0100

    Integrations test for adding tasks nearly done
    Relates #1

--- test.js
+++ test.js
@@ -1,43 +1,46 @@
 // Test 1. Add tasks to a list so that I can keep track of them.
 test("Submitting a new task adds it to the list", (t) => {
-  var actual = task();
-  var expected = [];
-  t.equal(actual, expected, "new task should add to list");
+  const input = document.querySelector(".header__input");
+  input.value = "change underpants";
+  const submit = document.querySelector(".header__button-add");
+  submit.click();
+  const result = document.querySelector(".taskText");
+  const expected = "change underpants";
+  t.equal(result.textContent, expected);
+  // result.textContent = "";
   // t.end();
   // test goes here
 });
 
-test("Adds item to list", (t) => {
-  var actual = 
-  var expected =
-    t.equal(actual, expected, "new task should add to list")
-})
-
+// test("Adds item to list", (t) => {
+//   const actual;
+//   const expected;
+//     t.equal(actual, expected, "new task should add to list")
+// })
 
 test("Creates an unordered list", (t) => {
   const main = document.querySelector("main");
-  var actual = main.innerHTML;
-  var expected = '<ul id="toDoList"></ul>';
-  actual === expected
-    ? console.info("list item is created")
-    : console.error(
-        `Expected: <ul id="toDoList"></ul>, Actual: ${main.innerHTML}`
-      );
-  // test goes here
+  const result = main.innerHTML;
+  const expected = '<ul id="toDoList"></ul>';
+  // result === expected
+  //   ? console.info("list item is created")
+  //   : console.error(
+  //       `Expected: <ul id="toDoList"></ul>, Actual: ${main.innerHTML}`
+  //     );
+  t.equal(result, expected);
 });
 
 test("Creates a task item", (t) => {
   const taskList = document.querySelector(".toDoList");
-  var actual = createTask("Create a presentation for work");
-  var expected = `<li class="task"><input type="checkbox" class="checkBox"><p class="taskText">
+  const result = createTask("Create a presentation for work");
+  const expected = `<li class="task"><input type="checkbox" class="checkBox"><p class="taskText">
                   Create a presentation for work
                   </p><button class="deleteButton">delete</button></li> `;
-  actual === expected
-    ? console.info("task is created")
-    : console.error("Task is not created");
-  // test goes here
+  // result === expected
+  //   ? console.info("task is created")
+  //   : console.error("Task is not created");
+  t.equal(result, expected);
 });
-
 
 // Test 2. Check things off my list so that I can see what I’ve done.
 test("Checking an entry marks it as complete", (t) => {
